4a7f6f060b57db6ccd443470c80f647e5c42ee57
MineDev_HD <96976141+MineDevHDnet@users.noreply.github.com>
2022-11-29 15:15:16 +0100

    Delete src/main/java/net/minedevhd/mineutil directory

--- src/main/java/net/minedevhd/mineutil/settings/UtilCore.java
+++
@@ -1,9 +0,0 @@
-package net.minedevhd.mineutil.settings;
-
-import net.minedevhd.mineutil.MineUtil;
-
-public interface UtilCore {
-	
-	final MineUtil mineUtil = MineUtil.getCore();
-
-}
--- src/main/java/net/minedevhd/mineutil/utils/graf/KeyCodes.java
+++
@@ -1,9 +0,0 @@
-package net.minedevhd.mineutil.utils.graf;
-
-public class KeyCodes {
-	
-	public static final Integer 
-		KEY_ENTER = 131,
-		KEY_DOT = 52;
-
-}
