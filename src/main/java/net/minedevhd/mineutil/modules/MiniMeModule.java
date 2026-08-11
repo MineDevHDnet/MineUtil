b727825b03d1f7023aef5d2eb706137403fb3cb5
MineDev_HD <96976141+MineDevHDnet@users.noreply.github.com>
2026-08-11 23:50:06 +0200

    Make MiniMe rendering world-safe

--- src/main/java/net/minedevhd/mineutil/modules/MiniMeModule.java
+++ src/main/java/net/minedevhd/mineutil/modules/MiniMeModule.java
@@ -1,77 +1,62 @@
 package net.minedevhd.mineutil.modules;
 
-import java.awt.Color;
-import java.util.UUID;
-
-import com.mojang.authlib.GameProfile;
-
 import net.labymod.ingamegui.ModuleCategory;
-import net.labymod.ingamegui.ModuleCategoryRegistry;
 import net.labymod.ingamegui.moduletypes.SimpleModule;
-import net.labymod.main.LabyMod;
 import net.labymod.settings.elements.ControlElement;
-import net.labymod.settings.elements.ControlElement.IconData;
 import net.labymod.utils.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.gui.inventory.GuiInventory;
-import net.minecraft.client.network.NetHandlerPlayClient;
-import net.minedevhd.mineutil.MineUtil;
-import net.minedevhd.mineutil.command.CommandManager;
-import net.minedevhd.mineutil.helper.TrustedUserHelper;
 import net.minedevhd.mineutil.settings.UtilCore;
-import net.minedevhd.mineutil.utils.ModButton;
-import net.minedevhd.mineutil.utils.graf.RenderUtils;
 
-public class MiniMeModule extends SimpleModule implements UtilCore {
+public final class MiniMeModule extends SimpleModule implements UtilCore {
 
-	@Override
-	public String getDefaultValue() {
-		return "Mini Me";
-	}
+    @Override
+    public String getDefaultValue() {
+        return "Mini Me";
+    }
 
-	@Override
-	public String getDisplayName() {
-		return "Mini Me";
-	}
+    @Override
+    public String getDisplayName() {
+        return "Mini Me";
+    }
 
-	@Override
-	public String getDisplayValue() {
-		try {
-			int x = (int) getLastX(), y = (int) getLastY();
-			GuiInventory.drawEntityOnScreen(x + 5, y + 60, 25, 2, 2, mc.thePlayer);
-		} catch (Exception exception) {}
-    	return "";
-	}
+    @Override
+    public String getDisplayValue() {
+        if (mc == null || mc.thePlayer == null) {
+            return "";
+        }
 
-	@Override
-	public String getDescription() {
-		return "Display a cute mini me of your skin.";
-	}
+        final int x = (int) getLastX();
+        final int y = (int) getLastY();
+        GuiInventory.drawEntityOnScreen(x + 5, y + 60, 25, 2, 2, mc.thePlayer);
+        return "";
+    }
 
-	@Override
-	public ControlElement.IconData getIconData() {
-		return new ControlElement.IconData(Material.NAME_TAG);
-	}
+    @Override
+    public String getDescription() {
+        return "Display a mini version of your player skin.";
+    }
 
-	@Override
-	public String getSettingName() {
-		return "Mini Me";
-	}
+    @Override
+    public ControlElement.IconData getIconData() {
+        return new ControlElement.IconData(Material.NAME_TAG);
+    }
 
-	@Override
-	public int getSortingId() {
-		return 0;
-	}
+    @Override
+    public String getSettingName() {
+        return "Mini Me";
+    }
 
-	@Override
-	public void loadSettings() {}
-    
+    @Override
+    public int getSortingId() {
+        return 0;
+    }
+
+    @Override
+    public void loadSettings() {
+    }
+
+    @Override
     public ModuleCategory getCategory() {
         return mineUtil.MINEUTIL_CATEGORY;
     }
-
 }
